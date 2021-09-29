fe79e8b566be6f4dda627cae7dde861fb0592da4
搋兔 <naruto6404@hotmail.com>
2021-09-29 23:38:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="[
+      'w-screen h-screen',
+      'flex',
+      'justify-center items-center'
+    ]"
+  >
+    <SimpleCalendar />
+  </div>
+</template>
+
+<script>
+import SimpleCalendar from './components/SimpleCalendar.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    SimpleCalendar,
+  }
+}
+</script>
